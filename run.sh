6451c715be354cbbec89b69c3f3489977fcfea7b
ktanoooo <ktanoooo1112@gmail.com>
2022-03-06 00:54:57 +0900

    Fix bug

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ charset=$2
 yarn webpack
 
 curl "https://gist.githubusercontent.com/ktanoooo/54cb81d199560a006b61a2508aa0f5fb/raw/2347b95bb9f62d6d75267f30cef45ae66bc259a1/iconv-lite-encodings-list.json" > dist/encodings_list.json
-cp $xmlpath ./dist/KindleSyncMetadataCache.xml
+cp "${xmlpath}" ./dist/KindleSyncMetadataCache.xml
 
 mkdir -p output
 node ./dist/index.js $charset

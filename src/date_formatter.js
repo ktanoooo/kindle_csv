c356d43c8c7735e9a1c9142ca95d5c3b89b0ab35
ktanoooo <ktanoooo1112@gmail.com>
2022-03-06 01:17:09 +0900

    Update date_formatter

--- src/date_formatter.js
+++ src/date_formatter.js
@@ -1,8 +1,8 @@
 module.exports = () => {
   const now = new Date();
-  let y = now.getFullYear();
-  let m = String(now.getMonth() + 1).padStart(2, "0");
-  let d = String(now.getDate()).padStart(2, "0");
-  let hhmmss = String(now).split(" ")[4].split(":").join("");
+  const y = now.getFullYear();
+  const m = String(now.getMonth() + 1).padStart(2, "0");
+  const d = String(now.getDate()).padStart(2, "0");
+  const hhmmss = String(now).split(" ")[4].split(":").join("");
   return `${y}${m}${d}${hhmmss}`;
 };
